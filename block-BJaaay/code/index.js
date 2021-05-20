6e6f4fc18382a55517e60191102903910cbc55f5
ritik-cyber <ritikdhingra03@gmail.com>
2021-05-20 11:38:59 +0530

    done

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,36 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
 
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
 // Using `console.log` log all the od values from 1 to 10.
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
 
 // Calculate the sum of all numbers from 1 to 10.
 
+for (let i = 1; i < 10; i++) {
+  i = i + i;
+  console.log(i);
+}
+
 // Log all the values from 1 to 10 using while loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
