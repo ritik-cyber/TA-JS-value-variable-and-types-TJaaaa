89cde766b9d24bd6c862ab6c7f4427b32f17bc37
ritik-cyber <ritikdhingra03@gmail.com>
2021-05-20 14:29:44 +0530

    done

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,54 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt("enter a user name");
+alert(user);
+let userName = user;
+console.log(user, userName);
+let age = 22,
+  islearning = true;
+user = "jhon";
+console.log(user, userName);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = Number(prompt("enter the numA"));
+let numB = Number(prompt("enter the numB"));
+let sum = numA + numB;
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++)
+  if (j % 2 === 0) {
+    console.log(j);
+  }
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let i = 9; i >= 1; i--) {
+  console.log(i);
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+let i = 5;
+while (i >= 0) {
+  console.log(i);
+  i--;
+}
+
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+let j = 10;
+while (j >= 0) {
+  if (j % 2 === 0) console.log(j);
+  j--;
+}
 
 // Comparisoin
 
@@ -29,10 +62,19 @@ for (let j = 0; j <= 10; ____) console.log(j);
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
 
-
 Example:
 
 21, 21; // true
 "hello", -21; // false
 
 */
+let num1 = Number(prompt("enter a vlue"));
+let num2 = Number(prompt("enter a vlue"));
+
+if (num1 === num2) {
+  alert("true");
+} else if (num1 !== num2) {
+  alert("flase");
+} else {
+  alert("enetr a valid value");
+}
